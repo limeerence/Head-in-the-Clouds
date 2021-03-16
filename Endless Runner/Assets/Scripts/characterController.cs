60ac9f1fdc9e531ec49193379a8dfb78c59bf9b4
limeerence <40286520+limeerence@users.noreply.github.com>
2021-03-16 15:52:11 -0500

    Add new project files

---
+++ Endless Runner/Assets/Scripts/characterController.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class characterController : MonoBehaviour
+{
+    [SerializeField] private bool isGrounded = true;
+    [SerializeField] private GameController gameController;
+
+    private void Start()
+    {
+        //Debug.Log("Start");
+        if (!gameController)
+        {
+            gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+        }
+
+    }
+
+    private void Update()
+    {
+        //move forward constantly
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, 1) * gameController.speed * Time.deltaTime;
+        //jump
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * gameController.jumpIntensity, ForceMode.Impulse);
+        //get down from in air
+        if ((Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) && !isGrounded)
+            gameObject.GetComponent<Rigidbody>().AddForce(Vector3.down * gameController.jumpIntensity * 2, ForceMode.Impulse);
+        //Debug.Log("Update");
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //set player touching ground, allows for jumping
+        if (collision.gameObject.tag == "Platform")
+            isGrounded = true;
+        //reduce health for hitting obstacle
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            collision.gameObject.GetComponent<BoxCollider>().enabled = false;
+            Debug.Log("Obstacle Hit");
+            gameController.updateHealth(-10);
+        }
+        if (collision.gameObject.tag == "Death")
+        {
+            gameController.GameOver();
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        //player is in the air, disallows jumping until grounded again
+        if (collision.gameObject.tag == "Platform")
+            isGrounded = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //slows player
+        if (other.gameObject.tag == "Slow")
+        {
+            Debug.Log("Slow Debuff");
+            other.gameObject.GetComponent<MeshRenderer>().enabled = false;
+            other.gameObject.GetComponent<MeshCollider>().enabled = false;
+            gameController.speed -= 5;
+            gameController.speedText.text = "Speed : " + gameController.speed;
+            gameController.StartCoroutine("slowedPopup");
+        }
+
+        //gives player an extra life
+        if (other.gameObject.tag == "Health")
+        {
+            Debug.Log("1-UP");
+            other.gameObject.GetComponent<MeshRenderer>().enabled = false;
+            other.gameObject.GetComponent<MeshCollider>().enabled = false;
+            gameController.updateHealth(10);
+            gameController.StartCoroutine("healthPopup");
+        }
+
+        //skips current game level
+        if (other.gameObject.tag == "Skip")
+        {
+            Debug.Log("Skip Level");
+            other.gameObject.GetComponent<MeshRenderer>().enabled = false;
+            other.gameObject.GetComponent<MeshCollider>().enabled = false;
+            gameController.StartCoroutine("SkipLevel");
+        }
+    }
+
+}
